18af0c5c403269796fea3d566e49670b25eb3a2d
FreeStar <1064389584@qq.com>
2018-02-06 23:57:19 +0800

    学习

---
+++ app/src/main/java/freestar/vip/magnifier/MainActivity.java
@@ -0,0 +1,104 @@
+package freestar.vip.magnifier;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import vip.freestar.mylogger.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.iv)
+    ImageView iv;
+    @Bind(R.id.fl)
+    FrameLayout fl;
+    @Bind(R.id.save)
+    Button save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Logger.init();
+
+        Glide.with(this)
+                .load("http://img02.tooopen.com/images/20160509/tooopen_sy_161967094653.jpg")
+                .into(iv);
+
+    }
+
+    private void viewSaveToImage(View view) {
+        view.setDrawingCacheEnabled(true);
+        view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        view.setDrawingCacheBackgroundColor(Color.WHITE);
+
+        // 把一个View转换成图片
+        Bitmap cachebmp = loadBitmapFromView(view);
+
+        FileOutputStream fos;
+        String imagePath = "";
+        try {
+            // 判断手机设备是否有SD卡
+            boolean isHasSDCard = Environment.getExternalStorageState().equals(
+                    Environment.MEDIA_MOUNTED);
+            if (isHasSDCard) {
+                // SD卡根目录
+                File sdRoot = Environment.getExternalStorageDirectory();
+                File file = new File(sdRoot, Calendar.getInstance().getTimeInMillis() + ".png");
+                fos = new FileOutputStream(file);
+                imagePath = file.getAbsolutePath();
+            } else
+                throw new Exception("创建文件失败!");
+
+            cachebmp.compress(Bitmap.CompressFormat.PNG, 90, fos);
+
+            fos.flush();
+            fos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Logger.e("imagePath=" + imagePath);
+
+        view.destroyDrawingCache();
+    }
+
+    private Bitmap loadBitmapFromView(View v) {
+        int w = v.getWidth();
+        int h = v.getHeight();
+
+        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmp);
+
+        c.drawColor(Color.WHITE);
+        /** 如果不设置canvas画布为白色，则生成透明 */
+
+        v.layout(0, 0, w, h);
+        v.draw(c);
+
+        return bmp;
+    }
+
+    @OnClick(R.id.save)
+    public void onViewClicked() {
+        viewSaveToImage(fl);
+    }
+}
